34856f4337e3e9d18dd445c6468f8345b2969a3c
JBTesla <jor.rojasr@duocuc.cl>
2022-09-05 13:11:38 -0400

    implementacion de validadores

--- src/app/pages/forget-pass/forget-pass.page.ts
+++ src/app/pages/forget-pass/forget-pass.page.ts
@@ -9,15 +9,31 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./forget-pass.page.scss'],
 })
 export class ForgetPassPage implements OnInit {
-
+  usuarios: any[] = [];
   email: string;
 
   constructor(private toastController: ToastController, private router:Router,private usuarioService: UserService) { }
 
   ngOnInit() {
+    this.usuarios = this.usuarioService.obtenerUsuarios();
   }
 
-recuperar(){
-  //var usuarioLogin=
+recuperarPass(){
+  var validarEmail= this.usuarioService.validarCorreo(this.email)
+ if (validarEmail != undefined) {
+  if (validarEmail.email == this.email) {
+    alert('Se ah enviado un correo electronico de recuperacion');
+  }
+ }else{
+  this.tostadaError();
+ }
 }
+  //toast
+  async tostadaError() {
+    const toast = await this.toastController.create({
+      message: 'Correo no registrado',
+      duration: 3000
+    });
+    toast.present();
+  }
 }
